72a16bb9b9a8b32bfce3c777e7655c2498e6ed6b
shantal rhobi <shantal.rohbi@student.moringaschool.com>
2024-09-04 13:44:32 +0300

    complete lab

--- lib/car.py
+++ lib/car.py
@@ -2,3 +2,11 @@ from vehicle import Vehicle
 
 class Car(Vehicle):
     pass
+    def __init__(self, wheel_size, wheel_number):
+        super().__init__(wheel_size, wheel_number)
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+    def fill_up_tank(self):
+        return "filling up!"
